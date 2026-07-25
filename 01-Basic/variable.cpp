203c019b9befef60fe7e11d16f674a0b3505ce54
DFGGSE <asdasda2881@qq.com>
2026-07-25 14:14:54 +0800

    Add variable declaration and output in variable.cpp

---
+++ 01-Basic/variable.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int level = 1;
+    double hp = 100.0;
+    char grade = 'A';
+
+    cout << "Level: " << level << endl;
+    cout << "HP: " << hp << endl;
+    cout << "Grade: " << grade << endl;
+
+    return 0;
+}
