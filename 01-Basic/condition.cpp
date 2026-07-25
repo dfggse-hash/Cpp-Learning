deb2507c1c9616f3ea13f344dde7b35144213172
DFGGSE <asdasda2881@qq.com>
2026-07-25 14:17:15 +0800

    Add condition check for player health status

---
+++ 01-Basic/condition.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int hp = 0;
+
+    if(hp <= 0)
+    {
+        cout << "Player Dead" << endl;
+    }
+    else
+    {
+        cout << "Player Alive" << endl;
+    }
+
+    return 0;
+}
