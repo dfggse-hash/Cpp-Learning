66b28743452c1e1b578249bfabf8bce131736f0f
DFGGSE <asdasda2881@qq.com>
2026-07-25 14:16:12 +0800

    Add input/output functionality for player name

---
+++ 01-Basic/input_output.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string playerName;
+
+    cout << "Enter player name:";
+    cin >> playerName;
+
+    cout << "Welcome "
+         << playerName
+         << endl;
+
+    return 0;
+}
